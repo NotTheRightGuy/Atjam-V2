5dea20ff1a17cae847db2dfd433c2ecdd142c70a
harshita1611 <harshitabhamhani5@gmail.com>
2024-03-24 15:53:34 +0530

    file uploaded successfully

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -3,17 +3,32 @@ import React from "react";
 import { useState } from "react";
 
 function Page() {
+    const [message, setMessage] = useState('');
     const handleFileUpload = (e) => {
-        const file = e.target.files[0];
-        if (file) {
+        try {
+            const file = e.target.files[0];
+            if (!file) {
+                throw new Error('No file selected');
+            }
+            setMessage('File uploaded successfully');
             console.log(file);
+        } catch (error) {
+            setMessage(`Error: ${error.message}`);
         }
     };
 
     return (
         <div>
             <h1 className="text-7xl font-semibold font-space text-center animate-pulse">Schedule</h1>
-            <input type="file" accept=".pdf" onChange={handleFileUpload} />
+            <div className="mt-10 ml-10 h-screen">
+                <label className="font-space font-semibold text-2xl">
+                    Having new timetable? Upload here
+                </label>
+                <div className="mt-7">
+                    <input type="file" accept=".pdf" onChange={handleFileUpload}/>
+                    {message && <p className="text-green-500 ">{message}</p>} 
+                </div>
+            </div>
         </div>
     );
 }
